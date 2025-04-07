896c2e76ace9a200979af1abd423d30b97e7f39f
D. SIGANOS <147884676+DIMSGN@users.noreply.github.com>
2025-04-08 02:55:10 +0300

    initials

--- src/gr/aueb/cf/ch1/InitialsApp.java
+++ src/gr/aueb/cf/ch1/InitialsApp.java
@@ -6,15 +6,15 @@ package gr.aueb.cf.ch1;
  */
 public class InitialsApp {
     public static void main(String[] args) {
-        System.out.println("        **          *************** ");
-        System.out.println("       ** **         **             ");
-        System.out.println("      **   **         **            ");
-        System.out.println("     **     **         **           ");
-        System.out.println("    **       **         **          ");
-        System.out.println("   **         **       **           ");
-        System.out.println("  **           **     **            ");
-        System.out.println(" **             **   **             ");
-        System.out.println(" *****************  *************** ");
+        System.out.println("         **          *************** ");
+        System.out.println("        ** **         **             ");
+        System.out.println("       **   **         **            ");
+        System.out.println("      **     **         **           ");
+        System.out.println("     **       **         **          ");
+        System.out.println("    **         **       **           ");
+        System.out.println("   **           **     **            ");
+        System.out.println("  **             **   **             ");
+        System.out.println(" ******************  *************** ");
 
     }
 }
