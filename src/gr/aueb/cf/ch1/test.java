403128e61dc9589ebe29e0315f07102572ba50a2
D. SIGANOS <147884676+DIMSGN@users.noreply.github.com>
2025-04-13 11:49:04 +0300

    test diferent outputs.

---
+++ src/gr/aueb/cf/ch1/test.java
@@ -0,0 +1,17 @@
+package gr.aueb.cf.ch1;
+/** This program is for testing
+ * different ways of printing the output.
+ */
+public class test {
+    public static void main(String[] args) {
+        //declare
+        int i = 0;
+        //commands and prints
+        while (i < 5) {
+            //System.out.println(i); // Prints 0-4
+            i++; // i= i + 1;
+            //System.out.println(i); //
+        }//while close
+        System.out.println(i);
+    }//method close
+}//class close
